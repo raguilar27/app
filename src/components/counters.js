d3a57dd9318a21498b60f136be8f493c63b4b50d
Rogelio Aguilar <raguilar27@github.com>
2021-09-29 11:24:13 -0700

    updated counters

--- src/components/counters.js
+++ src/components/counters.js
@@ -3,11 +3,18 @@ import { useState } from "react";
 const Counters = () => {
   const [counter, setCounter] = useState(0);
 
+  const increment = () => setCounter(counter + 1);
+
+  const reset = () => setCounter(0);
+
   return (
     <div>
       <h1>{counter}</h1>
-      <button className="button1" onClick={() => setCounter(counter + 1)}>
+      <button className="button1" onClick={increment}>
         Increment
+      </button>
+      <button className="button1" onClick={reset}>
+        Reset
       </button>
     </div>
   );
