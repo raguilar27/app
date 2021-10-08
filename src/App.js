34a7ab4537940d06b2af9b1cdfd97a4e52ea9527
Rogelio Aguilar <raguilar27@github.com>
2021-10-08 01:24:52 -0700

    created phonebook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import Api from "./components/other/api";
 import Counters from "./components/other/counters";
-import Courses from "./components/part2/courseDir/Courses";
-import Unicafe from "./components/part1/unicafeDir/Unicafe";
 import Anecdotes from "./components/part1/anecdotesDir/Anecdotes";
+import Courses from "./components/part2/courseDir/Courses";
+import Phonebook from "./components/part2/phonebookDir/Phonebook";
+import Unicafe from "./components/part1/unicafeDir/Unicafe";
 
 function App() {
   return (
     <div className="App">
       {/*<Counters />
-      <br />
       <Api /> 
       <Anecdotes />
-      <Unicafe />*/}
-      <Courses />
+      <Unicafe />
+      <Courses />*/}
+      <Phonebook />
     </div>
   );
 }
---
+++ src/components/part2/phonebookDir/Phonebook.js
@@ -0,0 +1,9 @@
+const Phonebook = () => {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+    </div>
+  );
+};
+
+export default Phonebook;
