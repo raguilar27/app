a9b87b304bb538d3539e4e67702cac609cf3d8e5
Rogelio Aguilar <raguilar27@github.com>
2021-10-08 21:14:36 -0700

    completed phonebook layout

--- src/components/part2/phonebookDir/Phonebook.js
+++ src/components/part2/phonebookDir/Phonebook.js
@@ -1,7 +1,83 @@
+import { useState } from "react";
+
 const Phonebook = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    // names to
+    found
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterName = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  const found = persons.find(
+    (person) => person.name === newName || person.number == newNumber
+  );
+
   return (
     <div>
-      <h1>Phonebook</h1>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={newFilter} onChange={handleFilterName} />
+      </div>
+      <br />
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <br />
+      <h2>Numbers</h2>
+      {persons
+        .filter((person) => {
+          if (!newFilter) return true;
+          if (
+            person.name.includes(newFilter) ||
+            person.number.includes(newFilter)
+          )
+            return true;
+        })
+        .map((person) => (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ))}
     </div>
   );
 };
