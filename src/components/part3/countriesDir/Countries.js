4a840daeec64261f00c116272bb0b9d43ea0d6d5
Rogelio Aguilar <raguilar27@github.com>
2021-10-09 22:20:53 -0700

    allowed to filter country by official name

--- src/components/part3/countriesDir/Countries.js
+++ src/components/part3/countriesDir/Countries.js
@@ -1,7 +1,38 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <div>
-      <h1>Countries</h1>
+      <div>
+        find countries
+        <input value={query} onChange={handleQuery} />
+      </div>
+      <div>
+        {countries
+          .filter((country) => {
+            if (!query) return true;
+            if (country.name.official.includes(query)) return true;
+          })
+          .map((country) => (
+            <pre key={country.name.official}>
+              {JSON.stringify(country, 2, null)}
+            </pre>
+          ))}
+      </div>
     </div>
   );
 };
